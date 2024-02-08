9cace29b321c444374eab6864d7fb93beee1ef16
ajoy-code-dreamer <ajoy.code.dreamer@gmail.com>
2024-02-08 19:34:26 +0600

    calculator js logic added

--- js/script.js
+++ js/script.js
@@ -5,11 +5,45 @@ const btnKey= document.querySelectorAll(".key")
 
 let inputValue = ''
 
-btnKey.forEach(function(btn){
-    btn.addEventListener('click', function(e){
-        inputValue += e.target.innerText
-        disIn.innerHTML = inputValue
+// btnKey.forEach(function(btn){
+//     btn.addEventListener('click', function(e){
+//         if(e.target.innerText == 'AC'){
+//             disIn.innerHTML = '0'
+//             disOut.innerHTML = '0'
+//         }else if(e.target.innerText == '='){
+//             let result = eval(inputValue)
+//             disOut.innerHTML = result
+//         }
+//         else{
+//             inputValue += e.target.innerText
+//         disIn.innerHTML = inputValue
+//         }
+//     })
+// })
+btnKey.forEach(btn =>{
+    btn.addEventListener('click',(equal)=>{
+        let btnValue = btn.dataset.key;
+        if(btnValue == 'ac'){
+            disIn.innerHTML = '0'
+            disOut.innerHTML = '0'
+            inputValue = ""
+        }else if(btnValue == '='){
+            let result = eval(inputValue)
+            disOut.innerHTML = result
+        }else{
+            inputValue += btnValue
+            disIn.innerHTML = inputValue
+        }
+        
     })
 })
+
+// function validationInput(inputValue){
+//     if(inputValue.indexOf('.') == -1){
+//         return inputValue += '.'
+//     }
+// }
+
+
 
 
